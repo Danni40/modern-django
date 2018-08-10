160f5eee1dbd1237a7aa87f35af591a138af9ecd
Danni40 <dlewis2@live.maryville.edu>
2018-08-09 19:24:29 -0500

    user list displaying

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,5 @@
 from .base import *
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='yp6kgx9wxp#qlu2!)jx=@&!f@!92a6q-bb=_qq(x0)khsc784)')
+
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
+
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='yp6kgx9wxp#qlu2!)jx=@&!f@!92a6q-bb=_qq(x0)khsc784)')
---
+++ project/api/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf.urls import url, include
+from rest_framework import routers
+from project.api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
